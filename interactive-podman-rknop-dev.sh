30f37b77d3beedce8d2cb81d6e64469e4837ec64
Rob Knop (Nersc) <raknop@lbl.gov>
2026-05-18 11:09:46 -0700

    Bump version in interactive-podman-rknop-dev to 0.1.40

--- interactive-podman-rknop-dev.sh
+++ interactive-podman-rknop-dev.sh
@@ -36,5 +36,5 @@ podman-hpc run ${PODMAN_HPC_RUN_GPU} \
     --env SNPIT_CONFIG=/snpit_env/configs/rknop_dev_container_config.yaml \
     --annotation run.oci.keep_original_groups=1 \
     -it \
-    registry.nersc.gov/m4385/roman-snpit-env:${WHICHROMANENV:-cpu}-0.1.39 \
+    registry.nersc.gov/m4385/roman-snpit-env:${WHICHROMANENV:-cpu}-0.1.40 \
     /bin/bash
