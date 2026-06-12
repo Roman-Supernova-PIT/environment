c4d5f3854d501b35100ab70f3e9dffbcab1fca3b
Michael Wood-Vasey <wmwv@pitt.edu>
2026-06-12 21:06:52 +0000

    Add SMDC container launching script.

---
+++ smdc-apptainer-mwv-dev.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+# First time through creat your local venv copy with
+# python -m venv --system-site-packages /home/venv
+# source /home/venv/bin/activate
+
+# image=/data/snpit/roman-snpit-env-cpu-0.1.36.sif
+# image=/data/snpit/roman-snpit-env-cuda-dev-0.1.41.sif
+image=/data/snpit/roman-snpit-env-cpu-0.1.41.sif
+
+apptainer exec \
+    --overlay pit_overlay.img \
+    --cleanenv \
+    --bind "${PWD}:/home" \
+    --bind "${HOME}/secrets:/secrets:ro" \
+    --bind "/data/snpit:/data_snpit" \
+    --bind "/dev/shm:/snpit_temp" \
+    --bind "/data/snpit/sidecar_dia_out:/sidecar_dia_out:rw" \
+    --bind "/data/snpit/database_dirs_rknop_dev/images:/data/images:ro" \
+    --pwd /home \
+    --env SNPIT_DEFAULT_CONFIG=/home/environment/smdc_dev_config.yaml \
+    --env SNPIT_CONFIG=/home/environment/smdc_dev_config.yaml \
+    ${image} \
+    /bin/bash "${@}"
