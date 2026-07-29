422e9e7a9d880d45565c1fb69b5223d9856fe707
laldoroty <laldoroty@localhost>
2026-07-29 20:00:48 +0000

    add cpu launcher script

---
+++ smdc-phrostydev-apptainer-cpu.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# First time through creat your local venv copy with
+# python -m venv --system-site-packages /home/venv
+# source /home/venv/bin/activate
+
+# image=/data/snpit/roman-snpit-env-cpu-0.1.36.sif
+# image=/data/snpit/roman-snpit-env-cuda-dev-0.1.36.sif
+image=/data/snpit/roman-snpit-env-cpu-0.1.41.sif
+
+apptainer exec \
+    --overlay pit_overlay.img \
+    --cleanenv \
+    --bind "${PWD}:/home" \
+    --bind "${HOME}/snpit/secrets:/secrets:ro" \
+    --bind "/data/snpit/phrosty_dia_out:/dia_out_dir" \
+    --bind "/data/snpit/phrosty_lc_out:/lc_out_dir" \
+    --bind "/data/snpit:/data_snpit" \
+    --bind "/dev/shm:/snpit_temp" \
+    --bind "/data/snpit/sidecar_dia_out:/sidecar_dia_out:rw" \
+    --bind "/data/snpit/database_dirs_rknop_dev/images:/data/images:ro" \
+    --bind "/mnt/roman-science-east-2/snpit/scratch:/scratch" \
+    --bind "/home/rkessler:/rick:ro" \
+    --bind "/mnt/roman-science-east-2/snpit/snana+romanisim+romancal:/ricksims:ro" \
+    --pwd /home \
+    --env SNPIT_DEFAULT_CONFIG=/home/environment/smdc_dev_config.yaml \
+    --env SNPIT_CONFIG=/home/environment/smdc_dev_config.yaml \
+    ${image} \
+    /bin/bash "${@}"
