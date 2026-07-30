3c46b6a930f40e2f5b7c324053482bbff8fea754
Michael Wood-Vasey <wmwv@pitt.edu>
2026-07-30 17:41:11 +0000

    First steps toward setup/startup script and SMDC site YAML.

---
+++ smdc-interactive.sh
@@ -0,0 +1,7 @@
+# MWV.  I have my installation in a subdirectory ~/Roman/pipeline
+SNPIT_DIR=${HOME}/Roman/pipeline
+
+export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/environment/smdc_interactive_config.yaml
+export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
+
+export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
