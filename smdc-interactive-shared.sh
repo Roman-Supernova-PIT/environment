a831af69ab021ae439a9daba821a1b27ac4c2479
Michael Wood-Vasey <wmwv@pitt.edu>
2026-08-10 16:38:06 -0700

    Decided to make the shared venv a separate PR.

--- smdc-interactive-shared.sh
+++
@@ -1,6 +0,0 @@
-SNPIT_DIR=/data/snpit
-
-export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/environment/smdc_interactive_config.yaml
-export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
-
-export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
