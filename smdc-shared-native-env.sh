0a2da0e2257ebf132e8a3f3dead1636511f006d1
laldoroty <laldoroty@ip-10-20-1-79.us-east-2.compute.internal>
2026-08-10 23:45:59 +0000

    add instructions for running shared native env

---
+++ smdc-shared-native-env.sh
@@ -0,0 +1,7 @@
+# 2026-08-10, Lauren Aldoroty
+#
+# Activate a shared environment suitable for Roman SNPIT tools on SMDC
+# Currently targeted at photometry packages: sidecar, phrosty, campari
+#
+# You will run this command whenever you want to activate the shared environment:
+source /data/snpit/snpit-photometry/bin/activate
