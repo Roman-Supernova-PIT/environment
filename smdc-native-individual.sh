f695ca048d2806effab3ad9a2a8aa5ee3753db5e
Michael Wood-Vasey <wmwv@pitt.edu>
2026-08-11 15:40:27 -0700

    Add note about where to check out packages.

--- smdc-native-individual.sh
+++ smdc-native-individual.sh
@@ -9,3 +9,6 @@ export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
 
 # Create a package definition dir env to be able to look up location of photometry_test_data
 export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
+
+# Then if you want to use a particular package sidecar, phrosty, campari
+# check it out in ${SNPIT_DIR} and then "pip install -e ."
