65ed903a9337a2c0b27db8d4decff0608b260721
laldoroty <laurenaldoroty@gmail.com>
2026-08-12 18:44:30 +0000

    update exports

--- smdc-install-native-shared.sh
+++ smdc-install-native-shared.sh
@@ -9,9 +9,14 @@
 mkdir -p /mnt/roman-science-internal/snpit/${LOGNAME}/temp_dir
 
 if [ x"$DEV_STORAGE" = "x" ]; then
-    DEV_STORAGE=/mnt/roman-science-internal/snpit/users/${LOGNAME}/dev_storage
+    export DEV_STORAGE=/mnt/roman-science-internal/snpit/users/${LOGNAME}/dev_storage
 fi
 sg snpit -c "mkdir -p $DEV_STORAGE"
 
-source /data/snpit/snpit-photometry/bin/activate
 source /data/snpit/env/environment_checkout_for_native/smdc-native-shared.sh
+
+SNPIT_DIR=/data/snpit
+export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/configs/smdc_native_config.yaml
+export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
+
+export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
