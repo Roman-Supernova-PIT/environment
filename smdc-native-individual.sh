8ef3aa067d8f84d0ee2fb4a0fa86138d387fb0ed
laldoroty <laurenaldoroty@gmail.com>
2026-08-12 19:01:43 +0000

    CRDS paths

--- smdc-native-individual.sh
+++ smdc-native-individual.sh
@@ -7,6 +7,10 @@ source ${SNPIT_DIR}/snpit-photometry/bin/activate
 export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/environment/smdc_native_config.yaml
 export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
 
+# Set locations that CRDS wants
+export CRDS_SERVER_URL=https://roman-crds.stsci.edu
+export CRDS_PATH=${HOME}/crds_cache
+
 # Create a package definition dir env to be able to look up location of photometry_test_data
 export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
 
--- smdc-native-shared.sh
+++ smdc-native-shared.sh
@@ -3,4 +3,7 @@ SNPIT_DIR=/data/snpit
 export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/configs/smdc_native_config.yaml
 export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
 
+export CRDS_SERVER_URL=https://roman-crds.stsci.edu
+export CRDS_PATH=${HOME}/crds_cache
+
 export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
