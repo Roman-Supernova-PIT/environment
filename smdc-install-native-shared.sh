730946411881a67d60948dbffd0548047ac615ce
laldoroty <laurenaldoroty@gmail.com>
2026-08-12 18:53:17 +0000

    i cannot do anything right

--- smdc-install-native-shared.sh
+++ smdc-install-native-shared.sh
@@ -13,10 +13,5 @@ if [ x"$DEV_STORAGE" = "x" ]; then
 fi
 sg snpit -c "mkdir -p $DEV_STORAGE"
 
+source /data/snpit/snpit-photometry/bin/activate
 source /data/snpit/env/environment_checkout_for_native/smdc-native-shared.sh
-
-SNPIT_DIR=/data/snpit
-export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/configs/smdc_native_config.yaml
-export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
-
-export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
--- smdc-native-shared.sh
+++ smdc-native-shared.sh
@@ -1,6 +1,6 @@
 SNPIT_DIR=/data/snpit
 
-export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/environment_checkout_for_native/smdc_native_config.yaml
+export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/configs/smdc_native_config.yaml
 export SNPIT_CONFIG=${SNPIT_DEFAULT_CONFIG}
 
 export SNPIT_PHOTOMETRY_TEST_DATA_DIR=${SNPIT_DIR}/photometry_test_data
