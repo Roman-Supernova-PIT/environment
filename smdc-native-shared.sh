8f25d34103d5e750b784a1fc3899d5041bae9b58
laldoroty <laurenaldoroty@gmail.com>
2026-08-17 13:13:05 +0000

    mwv comments II

--- smdc-native-shared.sh
+++ smdc-native-shared.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+# 2026-08-10, Lauren Aldoroty
+#
+# Activate a shared environment suitable for Roman SNPIT tools on SMDC
+# Currently targeted at photometry packages: sidecar, phrosty, campari
+#
+# When you want to activate the shared environment, do this:
+# source /data/snpit/env/environment_checkout_for_native/smdc-native-shared.sh 
+
+mkdir -p /mnt/roman-science-internal/snpit/${LOGNAME}/temp_dir
+
+if [ x"$DEV_STORAGE" = "x" ]; then
+    export DEV_STORAGE=/mnt/roman-science-internal/snpit/users/${LOGNAME}/dev_storage
+fi
+sg snpit -c "mkdir -p $DEV_STORAGE"
+
 SNPIT_DIR=/data/snpit
 
 export SNPIT_DEFAULT_CONFIG=${SNPIT_DIR}/env/configs/smdc_native_config.yaml
