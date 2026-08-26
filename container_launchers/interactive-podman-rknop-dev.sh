b209c31cee391c5a126c46a9446328137dfa065f
Rob Knop <raknop@lbl.gov>
2026-08-26 09:03:44 -0700

    Fix /data directory in interactive_podman_rknop_dev.sh

--- container_launchers/interactive-podman-rknop-dev.sh
+++ container_launchers/interactive-podman-rknop-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 bash $(dirname $0)/launch_container.sh \
-     --bind /data=/pscratch/sd/m/masao/roman_snpit \
+     --bind /data=/pscratch/sd/m/masao/roman_snpit/database_dirs_rknop_dev \
      -d /configs/rknop_dev_container_config.yaml \
      -c /configs/rknop_dev_container_config.yaml \
      --require-system nersc \
